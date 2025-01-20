93fcc6cced71d03f85b9d64b0af76f4e42a1fae1
Jack <jack4mccgmail.comgit config --global user.email jack4mccgmail.com>
2025-01-20 16:05:43 +0100

    sql lab 3 done

--- LAB 3 Data Aggregation and Trans.sql
+++ LAB 3 Data Aggregation and Trans.sql
@@ -9,6 +9,9 @@ FROM film;
 SELECT AVG(length), CONCAT(length DIV 60, ' hours and ', length % 60, ' minutes') AS Time_Formatted
 FROM film;
 
+SELECT FLOOR(AVG(length) / 60) AS hours, ROUND(AVG(length) % 60) AS minutes
+FROM film;
+
 SELECT length, CONCAT(length DIV 60, ' hours and ', length % 60, ' minutes') AS Time_Formatted
 FROM film;
 
@@ -22,13 +25,38 @@ LIMIT 1;
 SELECT *
 FROM rental;
 
-SELECT DATE_FORMAT(rental_date, "%M %d, %Y") as rental_date2, month(5), day(5)
+ALTER TABLE rental
+ADD WEEKDAY varchar(50);
+
+UPDATE rental
+SET WEEKDAY = 
+WHERE DATE_FORMAT(rental_date, '%W'); 
+
+SELECT *, DATE_FORMAT(rental_date, '%M') AS MONTH, DATE_FORMAT(rental_date, '%W') AS WEEKDAY
+FROM rental;
+
+ALTER TABLE rental
+ADD day_type varchar(50);
+
+UPDATE rental
+SET day_type = "weekday"
+WHERE WEEKDAY = "Tuesday" "Monday", "Wednesday", "Thursday", "Friday"; 
+
+SELECT *, DATE_FORMAT(rental_date, '%M') AS MONTH, DATE_FORMAT(rental_date, '%W') AS WEEKDAY
 FROM rental
 LIMIT 20;
+
 
 SELECT title, COALESCE(rental_duration, "Not Available") AS duration
 FROM film
 ORDER BY title ASC;
+
+SELECT *
+FROM customer;
+
+SELECT *, CONCAT( first_name," ", last_name) AS full_name, SUBSTRING(email, 1, 3) AS email_characcters
+FROM customer
+ORDER BY last_name;
 
 SELECT *
 FROM film;
@@ -56,6 +84,8 @@ GROUP BY rating
 HAVING Avg_Duration > 120
 ORDER BY Avg_Duration DESC;
 
+SELECT DISTINCT last_name
+FROM actor;
 
 
 
